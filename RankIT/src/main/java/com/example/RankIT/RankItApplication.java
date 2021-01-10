f4d91e5ac52e9b730f2b4f7c00662f0492de2dd8
SkrodzkiPiotrek <66201466+SkrodzkiPiotrek@users.noreply.github.com>
2021-01-10 09:47:53 +0100

    Pioerwszy Commit

---
+++ RankIT/src/main/java/com/example/RankIT/RankItApplication.java
@@ -0,0 +1,13 @@
+package com.example.RankIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankItApplication.class, args);
+	}
+
+}
---
+++ RankIT/src/test/java/com/example/RankIT/RankItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RankIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
